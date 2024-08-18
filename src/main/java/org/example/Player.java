57fbb0e1aff1e51259891de5085b501e5a9ea477
Sebastian <lang.sebastian.006@gmail.com>
2024-08-18 16:05:46 +0200

    Update Player.java

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Player {
+    // Der Marker, den der Spieler verwendet, um seine Züge zu markieren
+    public char marker;
+
+    /**
+     * Konstruktor für die Klasse Player.
+     * Initialisiert den Spieler mit einem gegebenen Marker.
+     */
+    Player(char marker){
+        this.marker = marker;
+    }
+
+    // Gibt den Marker des Spielers zurück.
+    public char getMarker() {
+        return marker;
+    }
+}
