ba1a718515d5835f41800d14986bf060fa831069
pezzi <pezzi.hartlieb@gmail.com>
2024-08-18 16:59:14 +0200

    starts game in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the FH-Technikum Tic Tac Toe game!");
+        // Init two players, board and the game
+        Player playerX = new Player('X');
+        Player playerY = new Player('O');
+        TicTacToe game = new TicTacToe(playerX, playerY);
+
+        // Start the game
+        game.play();
     }
 }
